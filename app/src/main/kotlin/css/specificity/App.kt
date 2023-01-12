80d30445a439bff07e04770587fd49d3da01c007
klemo <matuskoklemo@gmail.com>
2023-01-12 16:38:05 +0100

    fix: add requirement for selector to be trimed before parsing

--- app/src/main/kotlin/css/specificity/App.kt
+++ app/src/main/kotlin/css/specificity/App.kt
@@ -12,12 +12,12 @@ typealias Specificity = Triple<Int, Int, Int>
 private val delimiter = """[\s>+~]+""".toRegex()
 
 fun getSpecificity(selector: String): Specificity {
-    val size = selector.split(delimiter).fold(0) { acc, subSelector -> acc + valueOf(subSelector) }
+    val size = selector.trim().split(delimiter).fold(0) { acc, subSelector -> acc + valueOf(subSelector) }
 
     return Specificity(0, 0, size)
 }
 
-fun valueOf(selector: String): Int = when (selector) {
+fun valueOf(selector: String): Int = when (selector.trim()) {
     "*" -> 0
     else -> 1
 }
